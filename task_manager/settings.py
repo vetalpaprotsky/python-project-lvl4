f7c18dbceb48f4ceabbaef5c28807b4d09fe326c
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2021-01-14 10:26:56 +0200

    add bootstrap4

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager.tasks',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
