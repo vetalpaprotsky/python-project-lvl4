b6256cf10147a04bfe389ee0e116c9661347b45f
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2021-01-23 10:18:02 +0200

    add delete view

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from .views import (
-    IndexView,
-    CreateView,
-    UpdateView,
-    delete,
-    LoginView,
-    LogoutView,
+    UserIndexView,
+    UserCreateView,
+    UserUpdateView,
+    UserDeleteView,
+    UserLoginView,
+    UserLogoutView,
 )
 
 app_name = 'users'
 
 urlpatterns = [
-    path('users/', IndexView.as_view(), name='index'),
-    path('users/create/', CreateView.as_view(), name='create'),
-    path('users/<int:pk>/update/', UpdateView.as_view(), name='update'),
-    path('users/<int:pk>/delete/', delete, name='delete'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/', UserIndexView.as_view(), name='index'),
+    path('users/create/', UserCreateView.as_view(), name='create'),
+    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='update'),
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='delete'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,21 +1,23 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect, render
 from django.utils.translation import gettext as _
-from django.views import generic
+from django.views.generic import ListView, View
+from django.views.generic.edit import DeleteView
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from .forms import UserForm
 from .mixins import UserLoginRequiredMixin, OwnerOnlyMixin
 
 
-class IndexView(generic.ListView):
+class UserIndexView(ListView):
     model = User
     template_name = 'users/index.html'
     context_object_name = 'users'
 
 
-class CreateView(generic.View):
+class UserCreateView(View):
     form_class = UserForm
     template_name = 'users/create.html'
     redirect_url_pattern = 'users:login'
@@ -34,7 +36,7 @@ class CreateView(generic.View):
         return render(request, self.template_name, {'form': form})
 
 
-class UpdateView(UserLoginRequiredMixin, OwnerOnlyMixin, generic.View):
+class UserUpdateView(UserLoginRequiredMixin, OwnerOnlyMixin, View):
     form_class = UserForm
     template_name = 'users/update.html'
     redirect_url_pattern = 'users:index'
@@ -53,16 +55,24 @@ class UpdateView(UserLoginRequiredMixin, OwnerOnlyMixin, generic.View):
         return render(request, self.template_name, {'form': form})
 
 
-def delete(request):
-    pass
+class UserDeleteView(UserLoginRequiredMixin, OwnerOnlyMixin, DeleteView):
+    model = User
+    success_url = reverse_lazy('users:index')
+    template_name = 'users/delete.html'
+    context_object_name = 'user'
+    success_message = _("User has been deleted")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, self.success_message)
+        return super().delete(request, *args, **kwargs)
 
 
-class LoginView(SuccessMessageMixin, auth_views.LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'users/login.html'
     success_message = _("You've been logged in")
 
 
-class LogoutView(auth_views.LogoutView):
+class UserLogoutView(LogoutView):
     info_message = _("You've been logged out")
 
     def dispatch(self, request, *args, **kwargs):
